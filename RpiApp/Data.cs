eeb7d7c3e4a38b781648762f774a579479a4fcea
fatrybl <fatrybl@yandex.ru>
2018-02-25 21:35:06 +0300

    commit all

---
+++ RpiApp/Data.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Raspberry.IO.GeneralPurpose;
+using Raspberry.IO.Components;
+
+namespace RpiApp
+{
+
+    }
+public class Data
+{
+    public class TempSensor : Data // класс датчика температуры
+    {
+        private IGpioConnectionDriver driver;
+        public bool TempSensorConnection(int PinNumber)
+        {
+            bool IsConnected = false;
+            try
+            {
+                GpioInputOutputBinaryPin TempPin = new GpioInputOutputBinaryPin(driver, ProcessorPin.Pin11);
+                Raspberry.IO.Components.Sensors.Temperature.Dht.Dht11Connection dht11Connection = new Raspberry.IO.Components.Sensors.Temperature.Dht.Dht11Connection(TempPin);
+                TempPin.AsInput();
+                dht11Connection.Start();
+                IsConnected = true;
+                Console.WriteLine("Connection has been established and Pin is declared ");
+            }
+
+            catch (Exception ex)
+            {
+                Console.WriteLine("Cant establish connection or declare Pin " + ex.Message);
+            }
+            finally
+            {
+                Console.WriteLine("work witch connections is finished ");
+            }
+
+            return IsConnected;
+
+        }
+        public double GetTempSensorData(GpioInputOutputBinaryPin TempPin)//считывает данные с датчика температуры, сохраняет последние 200 изменений в файл и ОЗУ.
+        {
+            double TempData = 0;
+            Console.WriteLine("Pin11 is connected and gets Data ");
+            bool d = false;
+            d = TempPin.Read();
+            if (d == true) { TempData = 1; }
+            else { TempData = 0; }
+            return TempData;
+        }
+        public bool WriteDataToFile(double[] Data)
+        {
+            bool IsWritten = false;
+            try
+            {
+                if (Data != null)
+                {
+                    StreamWriter writer = new StreamWriter("TempSensorData.txt", true);
+
+                    foreach (double element in Data)
+                    {
+                        writer.WriteLine(element.ToString());
+                    }
+                    IsWritten = true;
+                    writer.Flush();
+                    writer.Close();
+                }
+                else { Console.WriteLine(" NULL data came to WriteDataToFile function !"); }
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Can`t write in file " + ex.Message);
+            }
+            finally
+            {
+                if (IsWritten == true)
+                { Console.WriteLine("Work with file has been finished successfully "); }
+                else
+                {
+                    Console.WriteLine("Work with file has been finished unsuccessfully ");
+                };
+            }
+            return IsWritten;
+        }
+        public double[] ReadDataFromFile(string FileName)
+        {
+            bool IsRead = false;
+            try
+            {
+                if (File.Exists(FileName))
+                {
+                    double i;
+                    string[] StringsFromFile = File.ReadAllLines(FileName);
+                    List<double> DataFromFile = new List<double> { };
+                    foreach (string line in StringsFromFile)
+                    {
+                        Console.WriteLine("\t" + line);
+                        if (double.TryParse(line, out i))
+                        {
+                            DataFromFile.Add(i);
+                            foreach(double element in DataFromFile)
+                            {
+                                Console.WriteLine(element.ToString());
+                            }
+                        }
+                    }
+                    IsRead = true;
+
+                }
+                else { Console.WriteLine("the " + FileName + " does not exist"); }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Can`t read from  file because " + ex.Message);
+            }
+            finally
+            {
+                if (IsRead == true)
+                { Console.WriteLine("Work with file has been finished successfully "); }
+                else
+                {
+                    Console.WriteLine("Work with file has been finished unsuccessfully ");
+                };
+            }
+        }
+
+
+    }
+}
+    
+
---
+++ RpiApp/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RpiApp
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            //Application.EnableVisualStyles();
+            //Application.SetCompatibleTextRenderingDefault(false);
+            //Application.Run(new Form1());
+
+
+            Data.TempSensor tempSensor = new Data.TempSensor();
+            tempSensor.TempSensorConnection(11);
+            double[] array = { 0, 1.5, 2.46, 3, 4, 5 };
+            if (tempSensor.WriteDataToFile(array))
+            {
+                double[] arr = tempSensor.ReadDataFromFile("TempSensorData.txt");
+                foreach (double element in arr)
+                {
+                    Console.WriteLine(element + "\n");
+                }
+            }
+            else Console.WriteLine("sukablyat");
+        }
+    }
+}
